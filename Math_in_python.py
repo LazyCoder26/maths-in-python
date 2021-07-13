1618371d331cc944e359bcb5204ea8cdcec6fc51
ChillCoder26 <77568653+LazyCoder26@users.noreply.github.com>
2021-07-13 09:22:30 +0530

    Add files via upload

---
+++ Math_in_python.py
@@ -0,0 +1,29 @@
+#For reference, here’s a list of other math operations you can apply towards numbers:
+#Operators      Operation             Example
+#   **          Exponent             2 ** 3 = 8
+#   %           Modulus/Remainder    22 % 8 = 6
+#   //          Integer division     22 // 8 = 2
+#   /           Division             22 / 8 = 2.75
+#   *           Multiplication       3 * 3 = 9
+#   -           Subtraction          5 - 2 = 3
+#   +           Addition             2 + 2 = 4
+
+
+# this is a exampele of upper case
+
+# input values
+
+a = 2
+b = 3
+
+# input values
+
+# don't use quotation mark i mathmatical programme like this      // print ("a**b") // dont do this mistake
+
+print( a**b )
+print( a % b )
+print( a // b )
+print( a / b )
+print( a * b )
+print( a - b )
+print( a + b )
